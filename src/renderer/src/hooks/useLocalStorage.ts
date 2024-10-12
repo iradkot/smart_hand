05d54a4cb92b33003a16df696bfed46c2e6eb49f
iradkot <iradkot@users.noreply.github.com>
2024-10-13 00:23:36 +0300

    Update vite minor version + Minor refactor to useLocalStorage - improve its ts

--- src/renderer/src/hooks/useLocalStorage.ts
+++ src/renderer/src/hooks/useLocalStorage.ts
@@ -1,36 +1,30 @@
-// src/hooks/useLocalStorage.ts
-
 import { useState } from 'react';
 
-function useLocalStorage<T>(
-  key: string,
-  initialValue: T
-): [T, (value: T | ((val: T) => T)) => void] {
-  // Initialize the state
+function useLocalStorage<T>(key: string, initialValue: T): [T, (value: T | ((val: T) => T)) => void] {
   const [storedValue, setStoredValue] = useState<T>(() => {
     if (typeof window === 'undefined') {
+      console.log('Window is undefined, returning initial value:', initialValue);
       return initialValue;
     }
     try {
       const item = window.localStorage.getItem(key);
-      return item ? (JSON.parse(item) as T) : initialValue;
+      if (item === null || item === undefined) {
+        return initialValue;
+      } else {
+        return JSON.parse(item) as T;
+      }
     } catch (error) {
       console.warn(`Error reading localStorage key "${key}":`, error);
       return initialValue;
     }
   });
 
-  // Wrap setValue to handle functions and persist to localStorage
   const setValue = (value: T | ((val: T) => T)) => {
+    const valueToStore = value instanceof Function ? value(storedValue) : value;
     try {
-      // Allow value to be a function so we have the same API as useState
-      const valueToStore =
-        value instanceof Function ? value(storedValue) : value;
-      // Save state
-      setStoredValue(valueToStore);
-      // Save to localStorage
       if (typeof window !== 'undefined') {
         window.localStorage.setItem(key, JSON.stringify(valueToStore));
+        setStoredValue(valueToStore);
       }
     } catch (error) {
       console.warn(`Error setting localStorage key "${key}":`, error);
